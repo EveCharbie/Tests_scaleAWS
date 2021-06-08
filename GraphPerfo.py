65e936a9ac596407933c7bd58e2c0eb3d5dfbc04
EveCharbie <eve.charbonneau.1@umontreal.ca>
2021-06-08 15:01:41 -0400

    4

--- GraphPerfo.py
+++ GraphPerfo.py
@@ -6,6 +6,10 @@ n_threads = np.array([1, 2, 4, 8, 16, 32, 64])
 Times = np.array([438, 313, 146, 79, 49, 55, 35]) #s
 Names = ['t2.micro', 't3.micro', 'c5.xlarge', 'c5.2xlarge', 'c5.4xlarge', 'c5a.8xlarge', 'c5a.16xlarge']
 
+Times_threadRipper = np.array([399.4399435520172, 
+
+])
+
 plt.figure()
 plt.title('Max vrille 8DoFs (version article bioptim)')
 for i in range(7):
